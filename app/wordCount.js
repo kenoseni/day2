b93c878ce75f7946f7679e4798ea1c3cab2cb2d0
Olusola Oseni <kenolusola@gmail.com>
2017-03-14 23:27:30 +0100

    wordCount

---
+++ app/wordCount.js
@@ -0,0 +1,18 @@
+exports.words= function(str){
+  
+  var myWords = str.split(/\s+/);
+  var wordCount = {};
+  for(var i = 0; i<myWords.length; i++){
+    var eachWord = myWords[i];
+    console.log(eachWord)
+    if(wordCount[eachWord] === undefined){
+      wordCount[eachWord] = 1;
+    }else if(typeof wordCount[eachWord] !== 'string' || keyword){
+      wordCount[eachWord] += 1;
+    }else{
+      wordCount[eachWord]+=1;
+    }
+    
+  }
+  return wordCount;
+}
