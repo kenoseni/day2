671278860726a11445b5e44e6217cccbfa48d6ae
Olusola Oseni <kenolusola@gmail.com>
2017-03-15 09:10:07 +0100

    my first commit

--- app/wordCount.js
+++ app/wordCount.js
@@ -7,8 +7,8 @@ exports.words= function(str){
     console.log(eachWord)
     if(wordCount[eachWord] === undefined){
       wordCount[eachWord] = 1;
-    }else if(typeof wordCount[eachWord] !== 'string' || keyword){
-      wordCount[eachWord] += 1;
+    }else if(typeof wordCount[eachWord] !== 'number' ){
+      wordCount[eachWord] = 1;
     }else{
       wordCount[eachWord]+=1;
     }
